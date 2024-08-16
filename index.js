b4470dd309f335ced32abee6162da0c34ffdc852
Luis Suarez <luisfreiser@gmail.com>
2024-08-15 19:05:05 -0500

    Save

--- index.js
+++ index.js
@@ -18,38 +18,35 @@ app.get("/api/notes", (request, response) => {
   Note.find({}).then((notes) => response.json(notes));
 });
 
-//!Generamos un ID para la nueva nota.
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+// //!Generamos un ID para la nueva nota.
+// const generateId = () => {
+//   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+//   return maxId + 1;
+// };
 
 //!Ruta para crear una nueva nota en la base de datos.
 app.post("/api/notes", (request, response) => {
-  const note = request.body;
-  if (!note.content) {
+  const body = request.body; //!Obtenemos el body con los datos del objeto JSON de la solicitud.
+  if (!body.content) {
     return response.status(400).json({ error: "content missing" });
   }
-
-  const newNote = {
-    id: generateId(),
-    content: note.content,
-    important: typeof note.important !== "undefined" ? note.important : false, //!Definiendo por defecto el valor de 'important' como 'false'.
-  };
-  notes = notes.concat(newNote);
-
-  response.json(note);
+  //!Creamos una nueva nota y le pasamos los datos del objeto body.
+  const newNote = new Note({
+    content: body.content,
+    important: typeof body.important !== "undefined" ? body.important : false, //!Definiendo por defecto el valor de 'important' como 'false'.
+  });
+  newNote
+    .save() //!Guardamos la nueva nota en la base de datos.
+    .then((savedNote) => {
+      response.json(savedNote); //!Retornamos la nueva nota si se realizo correctamente.
+    });
 });
 
 //!Ruta para buscar una nota.
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = Note.find((note) => note.id === id);
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 //!Ruta para eliminar una nota.
@@ -59,6 +56,7 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end(); //!204 = La solicitud HTTP ha sido completada.
 });
 
+//!Iniciamos el servidor.
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
